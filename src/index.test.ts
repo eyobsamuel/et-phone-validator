ade09d8b4647df15f5c96971ab508f7380a68b17
Eyob Samuel <eysamuel@excellerentsolutions.com>
2023-04-26 21:48:12 +0300

    phone number are modified

---
+++ src/index.test.ts
@@ -0,0 +1,144 @@
+import etPhone from "./index";
+
+describe('et-phone will validate', () => {
+  test("ethiopian ethio-telecom mobile phone numbers", () => {
+    expect(etPhone("+251 911 23 45 67")).toMatchObject({
+      isValid: true,
+      phoneNumber: "+251911234567",
+      provider: "Ethio telecom",
+      phoneType: "Mobile",
+    });
+    expect(etPhone("251 911 23 45 67")).toMatchObject({
+      isValid: true,
+      phoneNumber: "+251911234567",
+      provider: "Ethio telecom",
+      phoneType: "Mobile",
+    });
+    expect(etPhone("0911 23 45 67")).toMatchObject({
+      isValid: true,
+      phoneNumber: "+251911234567",
+      provider: "Ethio telecom",
+      phoneType: "Mobile",
+    });
+    expect(etPhone("911 23 45 67")).toMatchObject({
+      isValid: true,
+      phoneNumber: "+251911234567",
+      provider: "Ethio telecom",
+      phoneType: "Mobile",
+    });
+    expect(etPhone("0911 23 45 678")).toMatchObject({
+      isValid: false
+    });
+    expect(etPhone("911 23 45 678")).toMatchObject({
+      isValid: false
+    });
+    expect(etPhone("+2510911 23 45 67")).toMatchObject({
+      isValid: false
+    });
+  })
+
+  test("ethiopian safaricom mobile phone numbers", () => {
+    expect(etPhone("+251 711 23 45 67")).toMatchObject({
+      isValid: true,
+      phoneNumber: "+251711234567",
+      provider: "Safaricom",
+      phoneType: "Mobile",
+    });
+    expect(etPhone("251 711 23 45 67")).toMatchObject({
+      isValid: true,
+      phoneNumber: "+251711234567",
+      provider: "Safaricom",
+      phoneType: "Mobile",
+    });
+    expect(etPhone("711 23 45 67")).toMatchObject({
+      isValid: true,
+      phoneNumber: "+251711234567",
+      provider: "Safaricom",
+      phoneType: "Mobile",
+    });
+    expect(etPhone("0711 23 45 67")).toMatchObject({
+      isValid: true,
+      phoneNumber: "+251711234567",
+      provider: "Safaricom",
+      phoneType: "Mobile",
+    });
+    expect(etPhone("0711 23 45 678")).toMatchObject({ isValid: false });
+    expect(etPhone("711 23 45 678")).toMatchObject({ isValid: false });
+    expect(etPhone("+2510711 23 45 67")).toMatchObject({ isValid: false });
+  })
+
+  test("ethiopian ethio-telecom fixed-line phone numbers", () => {
+    expect(etPhone("0111111234")).toMatchObject({
+      isValid: true,
+      phoneNumber: "+251111111234",
+      provider: "Ethio telecom",
+      phoneType: "Fixed-line",
+    });
+    expect(etPhone("0221111234")).toMatchObject({
+      isValid: true,
+      phoneNumber: "+251221111234",
+      provider: "Ethio telecom",
+      phoneType: "Fixed-line",
+    })
+    expect(etPhone("0331101234")).toMatchObject({
+      isValid: true,
+      phoneNumber: "+251331101234",
+      provider: "Ethio telecom",
+      phoneType: "Fixed-line",
+    })
+    expect(etPhone("0344401234")).toMatchObject({
+      isValid: true,
+      phoneNumber: "+251344401234",
+      provider: "Ethio telecom",
+      phoneType: "Fixed-line",
+    })
+    expect(etPhone("0251111234")).toMatchObject({
+      isValid: true,
+      phoneNumber: "+251251111234",
+      provider: "Ethio telecom",
+      phoneType: "Fixed-line",
+    })
+    expect(etPhone("0461101234")).toMatchObject({
+      isValid: true,
+      phoneNumber: "+251461101234",
+      provider: "Ethio telecom",
+      phoneType: "Fixed-line",
+    })
+    expect(etPhone("0471111234")).toMatchObject({
+      isValid: true,
+      phoneNumber: "+251471111234",
+      provider: "Ethio telecom",
+      phoneType: "Fixed-line",
+    })
+    expect(etPhone("0572271234")).toMatchObject({
+      isValid: true,
+      phoneNumber: "+251572271234",
+      provider: "Ethio telecom",
+      phoneType: "Fixed-line",
+    })
+    expect(etPhone("0581111234")).toMatchObject({
+      isValid: true,
+      phoneNumber: "+251581111234",
+      provider: "Ethio telecom",
+      phoneType: "Fixed-line",
+    })
+  })
+
+  test("ethiopian ethio-telecom short code phone numbers", () => {
+    expect(etPhone("123", ["shortCode"])).toMatchObject({
+      isValid: true,
+      phoneNumber: "123",
+      provider: "Ethio telecom",
+      phoneType: "Short Code",
+    });
+  })
+
+  test("ethiopian ethio-telecom short code phone numbers", () => {
+    expect(etPhone("1234", ["special"])).toMatchObject({
+      isValid: true,
+      phoneNumber: "1234",
+      provider: "Ethio telecom",
+      phoneType: "Special",
+    });
+  })
+})
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,109 @@
-export default etPhone;
 /**
- * A phone validation function for Ethiopian ethio-telecom and Safari numbers that takes
+ * A phone validation function for Ethiopian Ethio-telecom and Safaricom numbers that takes
  * any phone number and type of a phone numbers,
  * and returns true when it is valid or false when it is invalid.
  *
- * @param {any}     phone - A nested objects.
- * @param {string}  "all" | "mobile" | "home" | "special" | "shortCode" - A path string, for example `my[1].test.field`
- * @param {boolean} [shouldThrow=false] - Optionally throw an exception when nothing found
- *
+ * @param {string | number} - Phone number
+ * @param {"all" | "mobile" | "fixedLine" | "special" | "shortCode" []} [type= "mobile", "fixedLine"] - Phone number type
+ * @param {"all" | "ethioTelecom" | "safaricom" []} [provider="all"] - Phone number provider. 
+ * @returns {{isValid, phoneNumber, provider, phoneType, countryIso2, countryIso3, countryCode}} - Phone number information
+ * 
  */
 
-type PhoneType = "all" | "mobile" | "home" | "special" | "shortCode";
-type ProviderType = "all" | "ethioTelecom" | "safaricom";
-type PhoneTypes = Array<PhoneType>;
+import { addCountryCode, extractNumber, ethioTelecomFixedLineRegex, globalResponse, checkEthioTelecomFixedLine } from "./util"
 
 function etPhone(
   phone: string | number,
-  type: PhoneTypes = ["mobile", "home"],
-  provider: ProviderType = "all"
+  type: Array<"all" | "mobile" | "fixedLine" | "special" | "shortCode"> = ["mobile", "fixedLine"],
+  provider: Array<"all" | "ethioTelecom" | "safaricom"> = ["all"]
 ) {
-  const phoneNumRegex =
-    /^\+?(251)?[79]\d{8}$|^\+?(251)?[17]\d{8}$|^\+?09\d{8}$|^\+?07\d{8}$|^\+?7\d{8}$/;
-  const phoneNum = phone.toString().split(' ').join('').split('-').join('');
-  if (provider === "all") {
-    if (type.includes("mobile")) return phoneNumRegex.test(phoneNum);
-    else return false;
+  const phoneNum = extractNumber(phone.toString())
+  const phoneWithAreaCode = addCountryCode(phoneNum)
+  console.log(phoneNum, phoneWithAreaCode);
+  
+  const ethioTelecomMobileRegex = /^\+?(251)?9\d{8}$/;
+  const ethioTelecomSpecialRegex = /^\d{4}$/;
+  const ethioTelecomShortCodeRegex = /^\d{3}$/;
+  const safaricomMobileRegex = /^\+?(251)?7\d{8}$/;
+
+  if (
+    ethioTelecomMobileRegex.test(phoneWithAreaCode) &&
+    (type.includes("mobile") ||
+      type.includes("all")) &&
+    (provider.includes("all") || provider.includes("ethioTelecom"))
+  )
+    return {
+      isValid: true,
+      phoneNumber: phoneWithAreaCode,
+      provider: "Ethio telecom",
+      phoneType: "Mobile",
+      ...globalResponse
+    };
+  else if (
+    safaricomMobileRegex.test(phoneWithAreaCode) &&
+    (type.includes("mobile") ||
+      type.includes("all")) &&
+    (provider.includes("all") || provider.includes("safaricom"))
+  )
+    return {
+      isValid: true,
+      phoneNumber: phoneWithAreaCode,
+      provider: "Safaricom",
+      phoneType: "Mobile",
+      ...globalResponse
+    };
+  else if (
+    ethioTelecomSpecialRegex.test(phoneNum) &&
+    (type.includes("special") ||
+      type.includes("all")) &&
+    (provider.includes("all") || provider.includes("ethioTelecom"))
+  )
+    return {
+      isValid: true,
+      phoneNumber: phoneNum,
+      provider: "Ethio telecom",
+      phoneType: "Special",
+      ...globalResponse
+    };
+  else if (
+    ethioTelecomShortCodeRegex.test(phoneNum) &&
+    (type.includes("shortCode") ||
+      type.includes("all")) &&
+    (provider.includes("all") || provider.includes("ethioTelecom"))
+  )
+    return {
+      isValid: true,
+      phoneNumber: phoneNum,
+      provider: "Ethio telecom",
+      phoneType: "Short Code",
+      ...globalResponse
+    };
+  else if (
+    ethioTelecomShortCodeRegex.test(phoneNum) &&
+    (type.includes("shortCode") ||
+      type.includes("all")) &&
+    (provider.includes("all") || provider.includes("ethioTelecom"))
+  )
+    return {
+      isValid: true,
+      phoneNumber: phoneNum,
+      provider: "Ethio telecom",
+      phoneType: "Short Code",
+      ...globalResponse
+    };
+  else if (checkEthioTelecomFixedLine(phoneWithAreaCode, ethioTelecomFixedLineRegex)) {
+    return {
+      isValid: true,
+      phoneNumber: phoneWithAreaCode,
+      provider: "Ethio telecom",
+      phoneType: "Fixed-line",
+      ...globalResponse
+    };
   }
-  else
-    return false
+  else {
+    return { isValid: false };
+  }
 }
+
+export default etPhone;
+
